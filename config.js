c1bbe19cc5df2046be27e06dbb37e20068159423
p-g-test <44712598+p-g-test@users.noreply.github.com>
2018-11-14 14:31:54 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,23 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'deinetubennewsapp1',
+		wp_ws_url : 'https://www.deinetubenews.de/wp-appkit-api/deinetubennewsapp1',
+		wp_url : 'https://www.deinetubenews.de',
+		theme : 'DTN-Theme-IOS',
+		version : '1.0.0',
+		app_type : 'phonegap-build',
+		app_title : 'SchmenzelAndroidApp1',
+		app_platform : 'ios',
+		app_path: '',
+		gmt_offset : 1,
+		debug_mode : 'off',
+		auth_key : '~2O=9Zwgva^Js$E3,@qw]qUhfLv;5.m~Fm+OW]_I&r@/@^]_h#43*=Kf`i,2<HCz',
+		options : {"refresh_interval":60},
+		theme_settings : [],
+		addons : []
+	};
+
+});
